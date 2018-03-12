7561141d8ed074e8005178b5511eb03086e036f8
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-03-12 03:50:40 -0700

    Create dfgj .c

---
+++ dfgj .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+#include<conio.h>
+void main()
+{
+    char str[MAX_SIZE];
+    char * s = str;
+    int alphabets, digits, others;
+clrscr();
+    alphabets = digits = others = 0;
+    printf("Enter any string : ");
+    gets(str);
+    while(*s)
+    {
+        if((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+            alphabets++;
+        else if(*s>='0' && *s<='9')
+            digits++;
+        else
+            others++;
+
+        s++;
+    }
+
+    printf("Alphabets = %d\n", alphabets);
+    printf("Digits = %d\n", digits);
+    printf("Special characters = %d", others);
+    getch():
+    }
